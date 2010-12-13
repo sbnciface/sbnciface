341591ec41079eb2bcd257e745ce34ee868e0b7f
Conny Sjöblom <biohzn@mustis.org>
2010-12-13 15:13:21 +0000

    - Removed files from compiled

--- dwoo/compiled/template/sbnciface/test.tpl.d17.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* template head */
-/* end template head */ ob_start(); /* template body */ ;
-echo $this->scope["testString"];
- /* end template body */
-return $this->buffer . ob_get_clean();
-?>
