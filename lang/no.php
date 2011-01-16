8e290a692f927a92aed27016beaa733e89566c70
Conny Sjöblom <biohzn@mustis.org>
2011-01-16 13:40:26 +0000

    - Norwegian added - German added

---
+++ lang/no.php
@@ -0,0 +1,138 @@
+<?php
+/*
+ * $Id: en.php 36 2010-12-21 09:37:38Z biohzn $
+ *
+ * Copyright (C) 2010-2011 Conny Sjöblom <biohzn@mustis.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<?php
+/*
+ * Norwegian language file.
+*/
+?>
+<?php
+$lang = array(
+
+    'pageNotFound'          =>  'Page \'%s\' ikke funnet, vennligst sjekk url.',
+    'noAccessToPage'        =>  'Du har ikke tilgang til denne siden',
+
+    'username'              =>  'Brukernavn',
+    'password'              =>  'Passord',
+    'server'                =>  'Server',
+    'login'                 =>  'Logg inn',
+    'wrongUserPass'         =>  'Feil brukernavn eller passord.',
+
+    'status'                =>  'Status',
+    'user'                  =>  'Bruker',
+    'settings'              =>  'Innstillinger',
+    'server'                =>  'Server',
+    'authSettings'          =>  'Auth innstillinger',
+    'away'                  =>  'Away',
+    'channels'              =>  'Kanaler',
+    'log'                   =>  'Logg',
+
+    'vAdmin'                =>  'Vadmin',
+
+    'admin'                 =>  'Administrator',
+    'users'                 =>  'Brukere',
+    'addUser'               =>  'Legg til bruker',
+    'trustedIps'            =>  'Trusted ips',
+    'vhosts'                =>  'Vhosts',
+    'globalMsg'             =>  'Global melding',
+    'mainLog'               =>  'Mainlog',
+
+    'logout'                =>  'Logg ut',
+
+    'nickname'              =>  'Brukernavn',
+    'uptime'                =>  'Uptime',
+    'disconnected'          =>  'Disconnected',
+    'awaynick'              =>  'Away nick',
+    'awaymessage'           =>  'Away melding',
+    'realname'              =>  'Realname',
+    'traffic'               =>  'Traffic',
+    'sysnotices'            =>  'System merknader',
+
+    'saveChanges'           =>  'Lagre endringer',
+
+    'port'                  =>  'Port',
+    'vhost'                 =>  'Vhost',
+    'jump'                  =>  'Jump',
+
+    'authName'              =>  'Auth navn',
+    'authPass'              =>  'Auth passird',
+    'useAuth'               =>  'Auto-auth',
+    'yes'                   =>  'Ja',
+    'no'                    =>  'Nei',
+    'on'                    =>  'On',
+    'off'                   =>  'Off',
+
+    'quitAsAway'            =>  'Bruk quit som away',
+
+    'joinChannel'           =>  'Join kanal',
+    'channel'               =>  'Kanal',
+    'join'                  =>  'Join',
+    'part'                  =>  'Part',
+    'modes'                 =>  'Modes',
+    'action'                =>  'Actions',
+
+    'logEmpty'              =>  'Logg tom',
+    'emptyLog'              =>  'Tøm logg',
+
+    'ident'                 =>  'Ident',
+    'now'                   =>  'Nå',
+    'never'                 =>  'Alldri',
+    'lastSeen'              =>  'Sist sett',
+    'administrators'        =>  'Administrators',
+
+    'ifPasswordEmpty'       =>  'NOTE. Hvis passordfeltet står tomt, blir et tilfeldig passord generert.',
+    'addUser'               =>  'Legg til bruker',
+
+    'ip'                    =>  'Ip',
+
+    'message'               =>  'Melding',
+    'send'                  =>  'Send',
+
+    'userLimit'             =>  'Bruker grense',
+    'host'                  =>  'Host',
+
+    'changingOptions'       =>  'Endre alternativer for <b>%s</b>',
+    'access'                =>  'Access',
+    'realname'              =>  'Realname',
+    'serverPassword'        =>  'Server passord',
+
+    //Messages
+
+    'userAdded'             =>  'Bruker %s lagt til, med passord: %s',
+    'settingsSaved'         =>  'Innstillingene er lagret',
+    'reconnecting'          =>  'Reconnecter bouncer...',
+    'successfullyJoined'    =>  'Vellykket joinet kanal %s',
+    'successfullyParted'    =>  'Vellykket partet kanal %s',
+    'logErased'             =>  'Logg vellykket slettet',
+    'reallyDelete'          =>  'Vil du virkelig slette brukeren?',
+    'userDeleted'           =>  'Bruker %s vellykket slettet',
+    'messageSent'           =>  'Meldingen er sendt',
+    'trustIpAdded'          =>  'Trusted IP Lagt til',
+    'trustIpDeleted'        =>  'Trusted IP Fjernet',
+    'trustIpAlreadyAdded'   =>  'Trusted IP alerede lagt til',
+    'trustIpNotValid'       =>  'Den IP du oppgav er ikke gyldig',
+    'vhostAdded'            =>  'Ny vhost lagt til',
+    'vhostRemoved'          =>  'Vhost vellykket fjernet',
+    'limitNotNumerical'     =>  'Innskrevet grensen er ikke numeriske',
+    'invalidUser'           =>  'Bruker \'%s\' er ikke gyldig',
+
+    'autoAddIp'             =>  'Automatisk lagt til webserver (%s) til trusted IP \'s',
+);
+?>
